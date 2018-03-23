5751d0524bd7d3886e6c0091e9c4df1cd2e843f9
cowtard <ei12109@fe.up.pt>
2018-03-23 11:02:54 +0000

    Added Aircraft class.

--- AirScheduling/Aviation/Aircraft.cs
+++ AirScheduling/Aviation/Aircraft.cs
@@ -1,7 +1,34 @@
-﻿namespace AirScheduling.Aviation
+﻿using System;
+
+namespace AirScheduling.Aviation
 {
+    /// <summary>
+    /// The current problem only takes into consideration three different type of aircrafts
+    /// </summary>
+    public enum AircraftType
+    {
+        Light,
+        Medium,
+        Heavy
+    }
+    
     public class Aircraft
     {
-        
+
+        private AircraftType _aircraftType;
+        private double _maxSpeed, _optimalSpeed, _minSpeed;
+        private DateTime _timeOfNextFlight;
+        private bool _emergency;
+
+        public Aircraft(AircraftType aircraftType, double maxSpeed, double optimalSpeed, double minSpeed, 
+            DateTime timeOfNextFlight, bool emergency)
+        {
+            _aircraftType = aircraftType;
+            _maxSpeed = maxSpeed;
+            _optimalSpeed = optimalSpeed;
+            _minSpeed = minSpeed;
+            _timeOfNextFlight = timeOfNextFlight;
+            _emergency = emergency;
+        }
     }
 }
