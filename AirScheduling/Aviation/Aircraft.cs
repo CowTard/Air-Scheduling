70c1fb1d0d5643f203bc079f3d1a4b7ceca0403c
cowtard <ei12109@fe.up.pt>
2018-03-29 12:09:21 +0100

    Reading AircraftDatabase.csv and minor class structure updates

--- AirScheduling/Aviation/Aircraft.cs
+++ AirScheduling/Aviation/Aircraft.cs
@@ -11,21 +11,69 @@ namespace AirScheduling.Aviation
         Medium,
         Heavy
     }
-    
+
+    /// <summary>
+    /// Class holding information about a specific model of aircraft
+    /// </summary>
     public class Aircraft
     {
+        private string _model;
         private AircraftType _aircraftType;
         private double _maxSpeed, _optimalSpeed, _minSpeed;
-        private DateTime _timeOfNextFlight;
-        private bool _emergency;
-
-        public Aircraft(AircraftType aircraftType, double maxSpeed, double optimalSpeed, double minSpeed, 
-            DateTime timeOfNextFlight, bool emergency)
+        
+        /// <summary>
+        /// Constructor of the class Aircraft
+        /// </summary>
+        /// <param name="aircraftType">Type of Aircraft <see cref="AircraftType"/></param>
+        /// <param name="model">Model by which the aircraft is known</param>
+        /// <param name="maxSpeed">Max speed of this model in Knots</param>
+        /// <param name="optimalSpeed">The Stall speed of this model in knots</param>
+        /// <param name="minSpeed">The minimum speed of this model in knots</param>
+        public Aircraft(AircraftType aircraftType, string model, double maxSpeed, double optimalSpeed, double minSpeed)
         {
             _aircraftType = aircraftType;
+            _model = model;
             _maxSpeed = maxSpeed;
             _optimalSpeed = optimalSpeed;
             _minSpeed = minSpeed;
+        }
+        
+        /// <summary>
+        /// Static function responsible for converting a string into an AircraftType
+        /// </summary>
+        /// <param name="type_of_aircraft">A string found in AircraftDatabase.csv</param>
+        /// <returns>An AircraftType equivalent of the passed string</returns>
+        public static AircraftType convert_string_into_AicraftType(string type_of_aircraft)
+        {
+
+            if (type_of_aircraft == "Light")
+                return AircraftType.Light;
+            else if (type_of_aircraft == "Medium")
+                return AircraftType.Medium;
+            else
+                return AircraftType.Heavy;
+
+        }
+    }
+    
+    /// <summary>
+    /// Class that holds information about aircrafts that appear on airport's radar
+    /// </summary>
+    public class AircraftRadar
+    {
+        private Aircraft _aircraft;
+        private DateTime _timeOfNextFlight;
+        private bool _emergency;
+        
+        /// <summary>
+        /// Constructor of the class
+        /// </summary>
+        /// <param name="aircraft">Object of the class <see cref="Aircraft"/></param>
+        /// <param name="timeOfNextFlight">Time left in minutes for the next flight of this aircraft</param>
+        /// <param name="emergency">Whether aircraft is in emergengy mode or not </param>
+        public AircraftRadar(Aircraft aircraft, DateTime timeOfNextFlight, bool emergency)
+        {
+            _aircraft = aircraft;
             _timeOfNextFlight = timeOfNextFlight;
             _emergency = emergency;
         }
--- AirScheduling/Program.cs
+++ AirScheduling/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AirScheduling.Aviation;
 using GeneticSharp.Domain;
 using GeneticSharp.Domain.Crossovers;
 using GeneticSharp.Domain.Mutations;
@@ -10,8 +15,15 @@ namespace AirScheduling
 {
     internal class AirScheduling
     {
+        
+        private static List<Aircraft> _aircraftModels = new List<Aircraft>();
+        
+        
         public static void Main(string[] args)
         {
+
+            read_configuration_files();
+            /*
             var selection = new EliteSelection();
             var crossover = new OrderedCrossover();
             var mutation = new ReverseSequenceMutation();
@@ -26,7 +38,63 @@ namespace AirScheduling
             ga.Start();
 
             Console.WriteLine("Best solution found has {0} fitness.", ga.BestChromosome.Fitness);
+            */
+        }
+
+        /// <summary>
+        /// Function responsible for handling the reading of configuration files as well as creating the varibles that
+        /// will be created as files are read
+        /// </summary>
+        /// <returns>True if no errors were found in the files, false otherwise</returns>
+        private static bool read_configuration_files()
+        {
+            try
+            {
+                var aircraftTypes = read_aircraft_database("../../Data/AircraftDatabase.csv");
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
             
+
+            return true;
+        }
+
+        /// <summary>
+        /// Reads and populates database of aircrafts
+        /// </summary>
+        /// <param name="fileUrl">Location of AircraftDatabase.csv</param>
+        /// <returns>True if no errors, false otherwise</returns>
+        private static bool read_aircraft_database(string fileUrl)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(fileUrl).Skip(1).ToArray();
+            
+                foreach (var line in lines)
+                {
+                    var splittedLine = line.Split(',');
+
+                    var model = splittedLine[1];
+                    var type = Aircraft.convert_string_into_AicraftType(splittedLine[2]);
+                    var minimumSpeed = Double.Parse(splittedLine[3]);
+                    var optimalSpeed = Double.Parse(splittedLine[4]);
+                    var maximumSpeed = Double.Parse(splittedLine[5]);
+                
+                    var aircraftSpecification = new Aircraft(type, model, minimumSpeed, optimalSpeed, maximumSpeed);
+                    AirScheduling._aircraftModels.Add(aircraftSpecification);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+            
+            return true;
         }
     }
 }
